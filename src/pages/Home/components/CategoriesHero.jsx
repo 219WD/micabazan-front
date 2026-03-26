11fc16e0785693f6ed80db624e51f20b560d6d4e
Juan Canepa <macgunmanmc@gmail.com>
2026-03-26 16:17:08 -0300

    correccion en las categorias

--- src/pages/Home/components/CategoriesHero.jsx
+++ src/pages/Home/components/CategoriesHero.jsx
@@ -8,18 +8,32 @@ const CategoriesHero = () => {
 
   const categories = [
     {
-      id: "bikinis",
+      id: "BIKINIS Y ENTERIZAS",
       label: "Bikinis y\nEnterizas",
       image: "https://micaelabazan.com/wp-content/uploads/elementor/thumbs/img_6616-scaled-ri8a91azfe8oectv6g16i7d2cc2ihlm5sfr0lxh4w0.jpeg",
-      filter: "Bikinis",
+      filter: "BIKINIS Y ENTERIZAS",
       description: "Diseños exclusivos para el agua",
     },
     {
-      id: "sportswear",
+      id: "INDUMENTARIA DEPORTIVA",
       label: "Indumentaria\nDeportiva",
       image: "https://micaelabazan.com/wp-content/uploads/elementor/thumbs/7ce480d0-0c3b-4d5b-8f8b-ec3e8ffda51a-rgj9mvioyvah52pmmxpce1kzua1ps00zqng1r9qkuo.jpeg",
-      filter: "Indumentaria Deportiva",
+      filter: "INDUMENTARIA DEPORTIVA",
       description: "Rendimiento y estilo",
+    },
+    {
+      id: "INDUMENTARIA CASUAL",
+      label: "Indumentaria\nCasual",
+      image: "https://micaelabazan.com/wp-content/uploads/elementor/thumbs/WhatsApp-Image-2026-02-03-at-17.19.01-rimka2w8zw91xb7xpuouv0rbm528wycwbwhgj1v1ts.jpeg",
+      filter: "INDUMENTARIA CASUAL",
+      description: "Estilo para el día a día",
+    },
+    {
+      id: "BEBES Y NIÑOS",
+      label: "Bebés y\nNiños",
+      image: "https://micaelabazan.com/wp-content/uploads/elementor/thumbs/WhatsApp-Image-2026-01-28-at-11.30.38-ribp4r6rnxz8jz4prmee6w77kk4e8dh1btabkidtds.jpeg",
+      filter: "BEBES Y NIÑOS",
+      description: "Moda para los más pequeños",
     },
   ];
 
